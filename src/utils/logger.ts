01e02ba75160b73d0ce708da99e6348fd13a2883
broki <mateusz.brokos1@gmail.com>
2026-01-05 12:51:49 +0100

    fixed logger file location

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -7,12 +7,19 @@ function getCallerFile(): string {
   const stack = err.stack?.split('\n') || [];
 
   for (const line of stack) {
-    if (!line.includes('logger.ts') && line.includes(process.cwd())) {
-      const match = line.match(/\((.*):\d+:\d+\)/);
+    if (!line.includes('logger')) {
+      const match =
+        line.match(/\((.*):\d+:\d+\)/) || line.match(/at (.*):\d+:\d+/);
       if (match && match[1]) {
         let filePath = match[1];
-        filePath = filePath.replace(/^.*?(src|dist)[\\/]/, '');
-        return filePath.replace(/\\/g, '/');
+        filePath = filePath.replace(/\\/g, '/');
+
+        const idxSrc = filePath.indexOf('src/');
+        const idxDist = filePath.indexOf('dist/');
+
+        if (idxSrc > 0) filePath = filePath.slice(idxSrc + 4);
+        else filePath = filePath.slice(idxDist + 5);
+        return filePath;
       }
     }
   }
@@ -22,15 +29,23 @@ function getCallerFile(): string {
 export const logger = {
   info: (...args: any[]) =>
     console.log(`🚀 ${chalk.blue(`[${getCallerFile()}]`)}`, ...args),
+
   warn: (...args: any[]) =>
     console.log(`⚠️ ${chalk.yellow(`[${getCallerFile()}]`)}`, ...args),
+
   success: (...args: any[]) =>
     console.log(`✅ ${chalk.green(`[${getCallerFile()}]`)}`, ...args),
+
   error: (...args: any[]) =>
     console.log(`❌ ${chalk.red(`[${getCallerFile()}]`)}`, ...args),
 
   debug: (...args: any[]) => {
     if (!DEBUG_MODE) return;
     console.log(`🐛 ${chalk.gray(`[${getCallerFile()}]`)}`, ...args);
+  },
+
+  color: (color: string, msg: string) => {
+    if ((chalk as any)[color]) return (chalk as any)[color](msg);
+    return msg;
   },
 };
